c56e706dd90b188aa693daa6d119f9b96cf499e4
deepaknayani22 <64520527+deepaknayani22@users.noreply.github.com>
2023-02-26 21:45:02 -0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+REGION_NAME = "us-east-1"
+INSTANCE_TYPE = 't2.micro'
+S3_INPUT = "s3-sce546-input-bucket"
+S3_OUTPUT = "s3-sce546-output-bucket"
+SQS_INPUT = "SQS_Input"
+SQS_OUTPUT = "SQS_Output"
+KEY = "AKIAQTHUAVLWLOYZA6NR"
+KEY_PASS = "GvgD6iDzaGA2NFqy5up9rCMnTY3HxRWAdz9kqlKt"
