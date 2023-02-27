dcf4a3623d87d5955a602eb0d53a25414a7e7754
deepaknayani22 <64520527+deepaknayani22@users.noreply.github.com>
2023-02-26 21:59:41 -0700

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-REGION_NAME = "us-east-1"
-INSTANCE_TYPE = 't2.micro'
-S3_INPUT = "s3-sce546-input-bucket"
-S3_OUTPUT = "s3-sce546-output-bucket"
-SQS_INPUT = "SQS_Input"
-SQS_OUTPUT = "SQS_Output"
-KEY = "AKIAQTHUAVLWLOYZA6NR"
-KEY_PASS = "GvgD6iDzaGA2NFqy5up9rCMnTY3HxRWAdz9kqlKt"
